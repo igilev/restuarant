f4bd56ff17d63456d5fb27c9b2e441ec00265e6e
Begimai Abdybekova <b.abdybekova@gmail.com>
2022-09-20 11:14:49 -0500

    rating

---
+++ src/components/Rating.jsx
@@ -0,0 +1,13 @@
+import { FaStar } from "react-icons/fa";
+import React from "react";
+const StarRating= () =>{
+return <div><FaStar size = {100}/></div>
+}
+
+
+function Rating(){
+  return(
+
+  )
+}
+export default Rating
