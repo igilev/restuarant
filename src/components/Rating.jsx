445a876effb413db2d4c7c3d3c4df9791801edd4
Begimai Abdybekova <b.abdybekova@gmail.com>
2022-09-20 11:21:39 -0500

    created rating component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,42 @@
 import { FaStar } from "react-icons/fa";
 import React from "react";
-const StarRating= () =>{
-return <div><FaStar size = {100}/></div>
-}
+import { useState } from "react";
 
+const StarRating = () => {
+  const [rating, setRating] = useState(null);
+  const [hovered, setHovered] = useState(null);
 
-function Rating(){
-  return(
+  return (
+    <div>
+      {[...Array(5)].map((star, index) => {
+        const ratingValue = index + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              size={25}
+              color={
+                ratingValue <= (hovered || rating) ? "darkorange" : "lightgrey"
+              }
+              className="star"
+              onMouseEnter={() => setHovered(ratingValue)}
+              onMouseLeave={() => setHovered(null)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
 
-  )
-}
-export default Rating
+// function Rating(){
+//   return(
+
+//   )
+// }
+export default StarRating;
